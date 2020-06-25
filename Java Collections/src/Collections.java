6ad9b41cc45859d232013c26a861b301d7c652ad
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-06-25 08:10:47 -0400

    Code commit
    Java collection notes

---
+++ Java Collections/src/Collections.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+
+/**
+ * @author aakarsh.rajagopalan
+ * Collections are a set of Interfaces and Classes that allow users to 
+ * leverage the various data structures that are available.
+ * 
+ * Collection Framework:
+ * This framework allows users to store and manipulate data within various
+ * data structures
+ * 
+ * For more information visit: https://www.javatpoint.com/collections-in-java
+ * 
+ * Interface vs classes:
+ * The difference between class and interface is that: An interface contains a bunch of methods 
+ * and variables, but they are all abstract. Meaning, an interface contains only the signature 
+ * of the method but not the body
+ */
+public class Collections {
+
+	/**
+	 * 
+	 */
+	public Collections() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
